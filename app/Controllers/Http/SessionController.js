a54031ddee2d1403d9b80ea5aa5f4f5990060b07
Arli Machado <arli.machado@gmail.com>
2019-06-08 23:42:51 -0300

    Validando a autenticacao pela origem do request e pelo tipo do usuário

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,9 +1,22 @@
 'use strict'
-
+const User = use('App/Models/User')
 class SessionController {
-  async store ({ request, auth }) {
-    const { email, password } = request.all()
+  async store ({ request, response, auth }) {
+    const { email, password, origin } = request.all()
     const token = await auth.attempt(email, password)
+
+    const user = await User.query()
+      .where('email', email)
+      .first()
+
+    if (
+      (origin === 'MOBILE' && user.type === 'ADMIN') ||
+      (origin === 'BROWSER' && user.type === 'USER')
+    ) {
+      return response
+        .status(401)
+        .send({ error: { message: 'Usuário não autorizado' } })
+    }
     return token
   }
 }
--- app/Models/User.js
+++ app/Models/User.js
@@ -1,9 +1,6 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-
-/** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
 
 class User extends Model {
@@ -18,15 +15,12 @@ class User extends Model {
   }
 
   /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
+   * Hide/Omit Fields From JSON Output
    */
+  static get hidden () {
+    return ['password', 'created_at', 'updated_at']
+  }
+
   tokens () {
     return this.hasMany('App/Models/Token')
   }
