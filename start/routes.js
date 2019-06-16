b10e9893e273f6ba28ccbe25a25185330e637ff3
Arli Machado <arli.machado@gmail.com>
2019-06-15 23:26:56 -0300

    Restringindo as rotas que precisam de token

--- start/routes.js
+++ start/routes.js
@@ -4,8 +4,11 @@ const Route = use('Route')
 Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store').validator('Auth')
 
-Route.post('files', 'FileController.store')
 Route.get('files/:id', 'FileController.show')
 
-Route.post('products', 'ProductController.store')
-Route.get('products', 'ProductController.index')
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+
+  Route.post('products', 'ProductController.store')
+  Route.get('products', 'ProductController.index')
+}).middleware(['auth'])
